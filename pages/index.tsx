da9e2d441a097a8e93cfa9806f7dbd234ef7e96c
Austin Booth <austinb@thestartupfactory.tech>
2021-12-17 17:47:51 +0000

    adds event listener and gets the number of tokens minted so far

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,13 @@ import twitterLogo from '../public/twitter-logo.svg'
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
-const TOTAL_MINT_COUNT = 50;
+const TOTAL_MINT_COUNT = 5;
+
+const CONTRACT_ADDRESS = "0x76C4585Dd5776095219a71131911e696a63dACc5";
 
 const Home: NextPage = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [totalNFTsMintedSoFar, setTotalNFTsMintedSoFar] = useState(0)
   
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -39,6 +42,9 @@ const Home: NextPage = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account)
+      // Setup listener! This is for the case where a user comes to our site
+      // and ALREADY had their wallet connected + authorized.
+      setupEventListener()
     } else {
       console.log("No authorized account found")
     }
@@ -62,15 +68,54 @@ const Home: NextPage = () => {
       * Boom! This should print out public address once we authorize Metamask.
       */
       console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]); 
+      setCurrentAccount(accounts[0]);
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // Get total NFTs minted so far...
+        const totalNFTsMintedSoFar = await connectedContract.getTotalNFTsMintedSoFar()
+        if (totalNFTsMintedSoFar > 0) {
+          setTotalNFTsMintedSoFar(totalNFTsMintedSoFar)
+        }
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", async (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          const newTotalNFTsMintedSoFar = await connectedContract.getTotalNFTsMintedSoFar()
+          setTotalNFTsMintedSoFar(newTotalNFTsMintedSoFar)
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on Rarible. Here's the link: https://rinkeby.rarible.com/token/${CONTRACT_ADDRESS}:${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error)
     }
   }
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0xAeA9eCeFE1596E31B681ad6C3E60A50f6d0EbE2c";
-  
     try {
       const { ethereum } = window;
   
@@ -84,7 +129,7 @@ const Home: NextPage = () => {
   
         console.log("Mining...please wait.")
         await nftTxn.wait();
-        
+        console.log(nftTxn);
         console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
   
       } else {
@@ -95,16 +140,24 @@ const Home: NextPage = () => {
     }
   }
 
-  // Render Methods
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
 
-  useEffect(() => {
-    checkIfWalletIsConnected();
-  }, [])
+  const renderMintUI = () => (
+    <>
+      <p className="sub-text">{`${totalNFTsMintedSoFar}/${TOTAL_MINT_COUNT} tokens minted so far`}</p>
+      <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+        Mint NFT
+      </button>
+    </>
+  )
 
   return (
     <div className="App">
@@ -114,13 +167,7 @@ const Home: NextPage = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {currentAccount === "" ? (
-            renderNotConnectedContainer()
-          ) : (
-            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
-              Mint NFT
-            </button>
-          )}
+          {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
         </div>
         <div className="footer-container">
           <Image alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
