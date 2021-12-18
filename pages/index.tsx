103cabdf7ad06675a18a31214bdbef7c35bafd12
Austin Booth <austinb@thestartupfactory.tech>
2021-12-18 22:49:12 +0000

    checks that the logged in users wallet is currently on the Rinkeby Test Network

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,6 +118,17 @@ const Home: NextPage = () => {
   const askContractToMintNft = async () => {
     try {
       const { ethereum } = window;
+
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log("Connected to chain " + chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4";
+      // IDs of other chains - https://docs.metamask.io/guide/ethereum-provider.html#chain-ids
+      if (chainId !== rinkebyChainId) {
+        alert("You are not connected to the Rinkeby Test Network!");
+        throw new Error('not connected to the Rinkeby Test Network')
+      }
   
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
